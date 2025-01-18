159deb99ade91c71944314eed389254577149c5c
Akshunna04 <akshunapatidar747@gmail.com>
2025-01-18 10:59:11 +0530

    java array code

---
+++ hello.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main
+{
+    static boolean mul(int arr[])
+    {
+        int max = arr[0], min = arr[0];
+        for(int num: arr)
+        {
+            if(num>max)
+            {
+                max = num;
+            }
+            if(num<min)
+            {
+                min = num;
+            }
+        }
+        if(max%min==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        boolean d = mul(array);
+        System.out.println("Are min and max multiples of each other: " + d);
+    } 
+}
